46fbc1a0ddb6f948cb788d87f0479038a1c39e85
ilya panin <ilya.panin@gmail.com>
2019-05-23 12:16:46 +0300

    reorganized vuex folder structure

---
+++ src/store/auth.js
@@ -0,0 +1,56 @@
+const state = {
+    user: null,
+    $auth: null    
+}
+
+const getters = {
+    displayName: state => {
+        if (state.user) {
+          return state.user.email
+        } else {
+          return ''
+        }
+      },
+      isAuthentificated: state => {
+        return state.user!=null      
+      }    
+}
+
+const mutations = {
+    user: (state, user) => {
+        state.user = user
+      },
+      auth: (state, auth) => {
+        state.$auth = auth
+      }    
+}
+
+const actions = {
+    signIn: ({ commit, state }, params) => {
+        const promise = state.$auth.signInWithEmailAndPassword(params.login, params.pass)
+        
+        promise
+            .then((user) => {
+                commit('user', user)
+                params.callBack()
+            })
+            .catch(err => {
+                params.callBack(err)
+            });       
+      },
+      signOut: ({ state }, params) => {
+        state.$auth.signOut()
+        params.callBack()
+      },
+      bindAuth: ({ commit }, auth) => {
+        auth.onAuthStateChanged(user => {
+           commit('user', user)
+        })
+        commit('auth', auth)
+  
+      }    
+}
+
+export default {
+    state, getters, mutations, actions
+}
